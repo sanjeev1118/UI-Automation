8085370c970d91ad1404fa8ad1ada33d399f6d6a
Sanjeev Kumar <kumarsanjeev1118@gmail.com>
2022-12-17 08:15:31 +0530

    new config

---
+++ src/main/java/com/app/Main.java
@@ -0,0 +1,11 @@
+package com.app;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println(Arrays.toString(new int[] {3,5,8,9,0}));
+    }
+}
