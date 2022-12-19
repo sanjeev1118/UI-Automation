9e78097a2bfcfef63adaa72ffe864a0cb390c3c4
Sanjeev Kumar <kumarsanjeev1118@gmail.com>
2022-12-19 18:35:56 +0530

    Writted program to find word count

---
+++ src/main/java/com/app/problem_conding/FindWordCount.java
@@ -0,0 +1,31 @@
+package com.app.problem_conding;
+
+import java.util.Scanner;
+
+public class FindWordCount {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter String to get word count..");
+        String str = scn.nextLine();
+        System.out.println(wordCountUsingSplitMethod(str));
+        System.out.println(wordCountWithoutUsingSplitMethod(str));
+    }
+
+    public static int wordCountUsingSplitMethod(String str) {
+        String[] arr = str.split(" ");
+        return arr.length;
+    }
+
+    public static int wordCountWithoutUsingSplitMethod(String str) {
+        if(str.isBlank())
+            return 0;
+
+        int count = 1;
+        for(int i=0; i<str.length()-1; i++) {
+            if((str.charAt(i) == ' ') && (str.charAt(i+1) != ' '))
+                count++;
+
+        }
+        return count;
+    }
+}
--- src/main/java/com/app/problem_conding/ReverseWord.java
+++ src/main/java/com/app/problem_conding/ReverseWord.java
@@ -1,2 +1,16 @@
-package com.app.problem_conding;public class ReverseWord {
+package com.app.problem_conding;
+
+public class ReverseWord {
+    public static void main(String[] args) {
+        String str = "This is a cow";
+        String[] arr = str.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=arr.length-1; i>=0; i--) {
+            sb.append(arr[i]);
+            sb.append(" ");
+        }
+        System.out.println("String Before: "+ str);
+        System.out.println("String After: "+ sb.toString());
+    }
 }
