386cf5ead0907fe0598a1302952507f309ef0c6f
Sanjeev Kumar <kumarsanjeev1118@gmail.com>
2022-12-26 18:40:55 +0530

    Added string problem

---
+++ src/main/java/com/app/domain/Employee.java
@@ -0,0 +1,7 @@
+package com.app.domain;
+
+public class Employee {
+    public String name;
+    public String address;
+    public int mobileNo;
+}
---
+++ src/main/java/com/app/problem_conding/LongestSubstring.java
@@ -0,0 +1,18 @@
+package com.app.problem_conding;
+
+public class LongestSubstring {
+    public static void main(String[] args) {
+        String str = "abcabcbb";
+        String firstsubstr = "";
+        String secondsubstr = "";
+
+        for(int i=0; i<str.length()-1; i++) {
+            for(int j=i+1; j<str.length(); j++) {
+                firstsubstr = str.substring(i,j);
+               if(firstsubstr.contains(""+str.charAt(j)) && firstsubstr.length()<secondsubstr.length())
+                   secondsubstr = firstsubstr;
+            }
+        }
+        System.out.println("Length of Longest Substring:- "+ firstsubstr.length());
+    }
+}
